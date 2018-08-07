b9a61baa00334e3f6f5311960d961ea817a22f50
Niyi Ogungbemi <niyiment@gmail.com>
2018-08-07 10:57:27 -0700

    Setting modified

--- employee/settings.py
+++ employee/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'employee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 } 
 
 
